bb719f48dd05d2605d6c0276f7797a788647d99e
Sergio Attanzio <falkon@live.it>
2025-02-06 14:17:36 +0100

    several changes to set a simple page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faWhatsapp, faAirbnb, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
+    // RouterOutlet,
     MatToolbarModule,
+    MatIconModule,
+    FontAwesomeModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'perla-d-occidente';
+  language: string = 'IT';
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faWhatsapp, faAirbnb, faInstagram, faFacebook, faMapMarkerAlt);
+    
+  }
+  
+  handlerLanguageChange() {
+    if(this.language === 'IT') {
+      this.language = 'EN';
+    } else {
+      this.language = 'IT';
+    }
+  }
 }
