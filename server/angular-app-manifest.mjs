deb153c2fdccfbb09578227abd45f007f200ce73
Sergio Attanzio <falkon@live.it>
2025-02-06 14:37:51 +0100

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23953, hash: '3d979edfb23a83cd00e86cf2874edcc8866f10182d43685d14b13edb599406a1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17527, hash: 'ae30312ecf0150194bb32f92bb65c68ea895a631053a91e99b6dbe5273f1e337', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 48786, hash: 'f7279090ceda0e567626e753d3fe3cf853be1eaa00d5540bcae8f438ef030744', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23953, hash: '172c3973eafc16b2527fef3466fb97d698103d70d6f4549049095334257ab479', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17527, hash: '745df3b6b05557f766e4c5baa4cca3ed3b6a290aa0b14b5617bd7e2e56f2d8b3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 48786, hash: '1e832ca185c7b70f3a4ca0426f2cb7267cb4bced123b8a22c6a7739bb3556f9d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-CXQUZ3PB.css': {size: 6979, hash: 'mYIPdabeAag', text: () => import('./assets-chunks/styles-CXQUZ3PB_css.mjs').then(m => m.default)}
   },
 };
